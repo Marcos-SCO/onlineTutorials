0a7ecbdea3fa5038197909263ee7a1d5539d8ace
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2019-12-29 01:13:00 -0300

    spinner contador

--- clickmove_18_12_2019/js/script.js
+++
@@ -1,8 +0,0 @@
-let slide = document.getElementById('slide');
-window.onclick = () => {
-    if (slide.style.flexGrow == 1){
-        slide.style.flexGrow = 2;
-    } else {
-        slide.style.flexGrow = 1;
-    }
-}
