98c48398c044aa3f2342f5d8f939a326269d7371
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2020-01-22 16:42:41 -0300

    404 not found parallax

---
+++ janeiro_2020/404_page_parallax_22_01_2020/js/script.js
@@ -0,0 +1,16 @@
+// Helper 
+
+function qSelect(item) {
+    return document.querySelector(item);
+}
+function qSelectAll(item) {
+    return document.querySelectorAll(item);
+}
+
+let container = qSelect('#container');
+window.onmousemove = (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+    container.style.backgroundPositionX = x + 'px';
+    container.style.backgroundPositionY = y + 'px';
+}
